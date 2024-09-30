31a2403aea61579a5cdc6d8dd91d4978fe803c05
Hammad-2 <hammad.manwar@gmail.com>
2024-09-30 12:23:24 +0500

    add currency converter

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currency: any = {
+    USD: 1,
+    PKR: 277.41,
+    INR: 83.78,
+    EUR: 0.90,
+    SAR: 3.75
+};
+
+let answer = await inquirer.prompt(
+    [
+        {
+        name: "from",
+        message: "Enter From Currency",
+        type: "list",
+        choices: ["USD", "PKR", "INR", "EUR", "SAR"],
+        },
+        {
+        name: "to",
+        message: "Enter To Currency",
+        type: "list",
+        choices: ["USD", "PKR", "INR", "EUR", "SAR"],
+        },
+        {    
+        name: "amount",
+        message: "Enter your Amount",
+        type: "number",
+        },
+    ],
+);
+
+let fromAmount = currency[answer.from];
+let toAmount = currency[answer.to];
+let amount = answer.amount;
+let baseAmount = amount / fromAmount; 
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
